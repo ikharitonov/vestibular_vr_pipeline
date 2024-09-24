4a32d10d50bdeff63bfc5023d04ad67ad2cd3dda
ikharitonov <sarfins2101@gmail.com>
2024-09-24 11:56:40 +0200

    harp folder renamed

--- harp_resources/utils.py
+++ harp_resources/utils.py
@@ -4,67 +4,12 @@ import pandas as pd
 from glob import glob
 from pathlib import Path
 import os
-import json
-from dotmap import DotMap
-from aeon.io.reader import Reader, Csv
-import aeon.io.api as api
-
-
-class SessionData(Reader):
-    """Extracts metadata information from a settings .jsonl file."""
-
-    def __init__(self, pattern="Metadata"):
-        super().__init__(pattern, columns=["metadata"], extension="jsonl")
-
-    def read(self, file):
-        """Returns metadata for the specified epoch."""
-        with open(file) as fp:
-            metadata = [json.loads(line) for line in fp] 
-
-        data = {
-            "metadata": [DotMap(entry['value']) for entry in metadata]
-        }
-        timestamps = [api.aeon(entry['seconds']) for entry in metadata]
-
-        return pd.DataFrame(data, index=timestamps, columns=self.columns)
-    
-
-class Video(Csv):
-    """Extracts video frame metadata."""
-
-    def __init__(self, pattern="VideoData"):
-        super().__init__(pattern, columns=["hw_counter", "hw_timestamp", "_frame", "_path", "_epoch"])
-        self._rawcolumns = ["Time"] + self.columns[0:2]
-
-    def read(self, file):
-        """Reads video metadata from the specified file."""
-        data = pd.read_csv(file, header=0, names=self._rawcolumns)
-        data["_frame"] = data.index
-        data["_path"] = os.path.splitext(file)[0] + ".avi"
-        data["_epoch"] = file.parts[-3]
-        data["Time"] = data["Time"].transform(lambda x: api.aeon(x))
-        data.set_index("Time", inplace=True)
-        return data
-
-
-def load_json(reader: SessionData, root: Path) -> pd.DataFrame:
-    root = Path(root)
-    pattern = f"{root.joinpath(root.name)}_*.jsonl"
-    data = [reader.read(Path(file)) for file in glob(pattern)]
-    return pd.concat(data)
-
+from aeon.io.reader import Reader
 
 def load(reader: Reader, root: Path) -> pd.DataFrame:
     root = Path(root)
     pattern = f"{root.joinpath(root.name)}_{reader.register.address}_*.bin"
     data = [reader.read(file) for file in glob(pattern)]
-    return pd.concat(data)
-
-
-def load_video(reader: Video, root: Path) -> pd.DataFrame:
-    root = Path(root)
-    pattern = f"{root.joinpath(root.name)}_*.csv"
-    data = [reader.read(Path(file)) for file in glob(pattern)]
     return pd.concat(data)
 
 def concat_digi_events(series_low: pd.DataFrame, series_high: pd.DataFrame) -> pd.DataFrame:
@@ -76,8 +21,8 @@ def concat_digi_events(series_low: pd.DataFrame, series_high: pd.DataFrame) -> p
 
 def get_register_object(register_number, harp_board='h1'):
     
-    h1_reader = harp.create_reader(f'h1-device.yml', epoch=harp.REFERENCE_EPOCH)
-    h2_reader = harp.create_reader(f'h2-device.yml', epoch=harp.REFERENCE_EPOCH)
+    h1_reader = harp.create_reader(f'harp_resources/h1-device.yml', epoch=harp.REFERENCE_EPOCH)
+    h2_reader = harp.create_reader(f'harp_resources/h2-device.yml', epoch=harp.REFERENCE_EPOCH)
     reference_dict = {
         'h1': {
             32: h1_reader.Cam0Event,
