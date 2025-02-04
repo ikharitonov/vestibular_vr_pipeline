462e65426e7a8d6020040b37f92ec418548a8739
ederancz <35807497+ederancz@users.noreply.github.com>
2025-02-04 13:44:18 +0100

    update utils.py

--- harp_resources/utils.py
+++ harp_resources/utils.py
@@ -1,15 +1,20 @@
-import harp
-import numpy as np
-import pandas as pd
+import os
+import json
+import time
 from glob import glob
 from pathlib import Path
-import os
-from time import time
-from aeon.io.reader import Reader, Csv, Harp
+
+import numpy as np
+import pandas as pd
 import h5py
-import json
 from dotmap import DotMap
+
+import harp
+from aeon.io.reader import Reader, Csv, Harp
 import aeon.io.api as api
+
+#FIXME list
+# very verbose, e.r. printing which dataset is loaded many times, giving how long it took (find and remove these time.time())
 
 class SessionData(Reader):
     """Extracts metadata information from a settings .jsonl file."""
@@ -17,7 +22,7 @@ class SessionData(Reader):
     def __init__(self, pattern):
         super().__init__(pattern, columns=["metadata"], extension="jsonl")
 
-    def read(self, file):
+    def read(self, file, print_contents=True):
         """Returns metadata for the specified epoch."""
         with open(file) as fp:
             metadata = [json.loads(line) for line in fp] 
@@ -28,6 +33,9 @@ class SessionData(Reader):
         timestamps = [api.aeon(entry['seconds']) for entry in metadata]
 
         return pd.DataFrame(data, index=timestamps, columns=self.columns)
+        # Pretty print if needed
+        if print_contents:
+            print(json.dumps(data, indent=4))
 
 
 class TimestampedCsvReader(Csv):
@@ -43,7 +51,7 @@ class TimestampedCsvReader(Csv):
         return data
     
 
-class PhotometryReader(Csv):#FIXME Not used? 
+class PhotometryReader(Csv):
     def __init__(self, pattern):
         #super().__init__(pattern, columns=["Time", "Events", "CH1-410", "CH1-470", "CH1-560", "U"], extension="csv")
         super().__init__(pattern, columns=["TimeStamp", "470_dfF", "410_dfF", "56_dfF"], extension="csv")
@@ -70,15 +78,14 @@ class Video(Csv):
         return data
 
 
-
-def load_2(reader: Reader, root: Path) -> pd.DataFrame: #reads multiple files matching a pattern and concatenates their contents into a single DataFrame.
+def load_2(reader: Reader, root: Path) -> pd.DataFrame: #was called load_vers2_3 in Hilde's branch
     root = Path(root)
     pattern = f"{root.joinpath(reader.pattern).joinpath(reader.pattern)}_*.{reader.extension}"
     data = [reader.read(Path(file)) for file in glob(pattern)]
     return pd.concat(data)
 
 
-def load(reader: Reader, root: Path) -> pd.DataFrame: #FIXME this is likely superseeded by load_2 and deprecated 
+def load(reader: Reader, root: Path) -> pd.DataFrame:
     root = Path(root)
     pattern = f"{root.joinpath(root.name)}_{reader.register.address}_*.bin"
     data = [reader.read(file) for file in glob(pattern)]
@@ -88,6 +95,7 @@ def load(reader: Reader, root: Path) -> pd.DataFrame: #FIXME this is likely supe
 def load_harp(reader: Harp, root: Path) -> pd.DataFrame:
     root = Path(root)
     pattern = f"{root.joinpath(root.name)}_{reader.register.address}_*.bin"
+    print(pattern)
     data = [reader.read(file) for file in glob(pattern)]
     return pd.concat(data)
 
@@ -118,6 +126,192 @@ def get_register_object(register_number, harp_board='h1'):
     }
     return reference_dict[harp_board][register_number]
 
+def read_ExperimentEvents(path):
+    filenames = os.listdir(path/'ExperimentEvents')
+    filenames = [x for x in filenames if x[:16]=='ExperimentEvents'] # filter out other (hidden) files
+    sorted_filenames = pd.to_datetime(pd.Series([x.split('_')[1].split('.')[0] for x in filenames])).sort_values()
+    read_dfs = []
+    try:
+        for row in sorted_filenames:
+            read_dfs.append(pd.read_csv(path/'ExperimentEvents'/f"ExperimentEvents_{row.strftime('%Y-%m-%dT%H-%M-%S')}.csv"))
+        return pd.concat(read_dfs).reset_index().drop(columns='index')
+    except pd.errors.ParserError as e:
+        filename = f"ExperimentEvents_{row.strftime('%Y-%m-%dT%H-%M-%S')}.csv"
+        print(f'Tokenisation failed for file "{filename}".\n')
+        print(f'Exact description of error: {e}')
+        print('Likely due to extra commas in the "Value" column of ExperimentEvents. Please manually remove and run again.')
+        return None
+    except Exception as e:
+        print('Reading failed:', e)
+        return None
+
+
+def read_OnixDigital(path, version=None):
+    # version refers to how they were saved: 1 means Cohort 0, 2 means Cohort 1 and 3 means Cohort 2 onwards
+    # but automatic version may work, that would be ideal 
+    
+    filenames = os.listdir(path/'OnixDigital')
+    filenames = [x for x in filenames if x[:11]=='OnixDigital'] # filter out other (hidden) files
+    sorted_filenames = pd.to_datetime(pd.Series([x.split('_')[1].split('.')[0] for x in filenames])).sort_values()
+    read_dfs = []
+
+    #Attempt to detect file version
+    if version is None:
+        if any('OnixDigital' in fname for fname in filenames):
+            version = "version1"
+            print('Detected: First Onix Digital version')
+        elif any('Clock' in fname for fname in filenames):
+            version = "version3"
+            print('Detected: New Onix Digital version')
+        else:
+            version = "version2"
+            print('Detected: Second Onix Digital version')
+    
+    for row in sorted_filenames: 
+        #Reading the data into csv:
+        #Version 1 and 2
+        if version == 'version1':
+            data = pd.read_csv(path/'OnixDigital'/f"OnixDigital_{row.strftime('%Y-%m-%dT%H-%M-%S')}.csv")
+            
+        if version == 'version2':
+            onix_digital_reader = utils.TimestampedCsvReader("OnixDigital", columns=["Clock", "HubClock", #FIXME we are inside utiils, so probably not needed, this is an error by copying from notebook?
+                                                                         "DigitalInputs0",
+                                                                         "DigitalInputs1",
+                                                                         "DigitalInputs2",
+                                                                         "DigitalInputs3",
+                                                                         "DigitalInputs4",
+                                                                         "DigitalInputs5"
+                                                                         "DigitalInputs6",
+                                                                         "DigitalInputs7",
+                                                                         "DigitalInputs8",
+                                                                         "Buttons"])
+            onix_harp_reader = utils.TimestampedCsvReader("OnixHarp", columns=["Clock", "HubClock", "HarpTime"])
+            
+            data = utils.load_vers2_3(onix_digital_reader, path)
+        
+        if version == 'version3':
+            onix_digital_reader = utils.TimestampedCsvReader("OnixDigital", columns=["Clock", "HubClock", "DigitalInputs", "Buttons"])
+            data = load_vers2_3(onix_digital_reader, path) # Ensure load_vers2_3 can take this path type Path()
+
+        #appending all dfs to a list
+        read_dfs.append(data)
+
+    
+    return pd.concat(read_dfs).reset_index().drop(columns='index')
+
+
+def read_OnixAnalogFrameCount(path):
+    filenames = os.listdir(path/'OnixAnalogFrameCount')
+    filenames = [x for x in filenames if x[:20]=='OnixAnalogFrameCount'] # filter out other (hidden) files
+    sorted_filenames = pd.to_datetime(pd.Series([x.split('_')[1].split('.')[0] for x in filenames])).sort_values()
+    read_dfs = []
+    for row in sorted_filenames:
+        read_dfs.append(pd.read_csv(path/'OnixAnalogFrameCount'/f"OnixAnalogFrameCount_{row.strftime('%Y-%m-%dT%H-%M-%S')}.csv"))
+    return pd.concat(read_dfs).reset_index().drop(columns='index')
+
+def read_OnixAnalogData(dataset_path, binarise=False):
+    # https://github.com/neurogears/vestibular-vr/blob/benchmark-analysis/Python/vestibular-vr/analysis/round_trip.py
+    # https://open-ephys.github.io/onix-docs/Software%20Guide/Bonsai.ONIX/Nodes/AnalogIODevice.html
+    start_time = time.time()
+    arrays_to_concatenate = []
+    files_to_read = [x for x in os.listdir(dataset_path/'OnixAnalogData')]
+    
+    def extract_number(filename):
+        return int(filename.split('_')[-1].split('.')[0])
+    
+    # Sort the files based on the extracted number
+    files_to_read.sort(key=extract_number)
+    
+    for filename in files_to_read:
+        with open(dataset_path/'OnixAnalogData'/filename, 'rb') as f:
+            photo_diode = np.fromfile(f, dtype=np.int16)
+
+            try:
+                photo_diode = np.reshape(photo_diode, (-1,12))
+            except:
+                print(f'ERROR: Cannot reshape loaded "{filename}" binary file into [-1, 12] shape. Continuing with non-reshaped data.')
+            
+            arrays_to_concatenate.append(photo_diode)
+
+    photo_diode = np.concatenate(arrays_to_concatenate)
+    
+    if binarise:
+        PHOTODIODE_THRESHOLD = 120
+        photo_diode[np.where(photo_diode <= PHOTODIODE_THRESHOLD)] = 0
+        photo_diode[np.where(photo_diode > PHOTODIODE_THRESHOLD)] = 1
+        photo_diode = photo_diode.astype(bool)
+
+    print(f'OnixAnalogData loaded in {time.time() - start_time:.2f} seconds.')
+
+    return photo_diode
+
+def read_OnixAnalogClock(dataset_path):
+    start_time = time.time()
+    arrays_to_concatenate = []
+    files_to_read = [x for x in os.listdir(dataset_path/'OnixAnalogClock')]
+    
+    def extract_number(filename):
+        return int(filename.split('_')[-1].split('.')[0])
+    
+    # Sort the files based on the extracted number
+    files_to_read.sort(key=extract_number)
+    
+    for filename in files_to_read:
+        with open(dataset_path/'OnixAnalogClock'/filename, 'rb') as f:
+            clock_data = np.fromfile(f, dtype=np.uint64)
+            arrays_to_concatenate.append(clock_data) 
+    
+    output = np.concatenate(arrays_to_concatenate)
+
+    print(f'OnixAnalogClock loaded in {time.time() - start_time:.2f} seconds.')
+
+    return output
+
+
+def read_SessionSettings(dataset_path, print_contents=False):
+    path = Path(dataset_path)
+    session_settings_path = path / 'SessionSettings'
+    
+    # List and filter only JSONL files
+    jsonl_files = [f for f in os.listdir(session_settings_path) if f.endswith('.jsonl')]
+    
+    if not jsonl_files:
+        raise FileNotFoundError(f"No .jsonl files found in {session_settings_path}")
+
+    jsonl_file_path = session_settings_path / jsonl_files[0]  # Pick the first valid .jsonl file, there should be only one!
+
+    # Open and read the jsonl file line by line
+    with open(jsonl_file_path, 'r', encoding="utf-8") as file:
+        for line in file:
+            line = line.strip()  # Remove whitespace/newlines
+            if not line: 
+                continue  # Skip empty lines
+
+            try:
+                data = json.loads(line)
+            except json.JSONDecodeError as e:
+                print(f"Skipping malformed JSON line: {line[:100]}... Error: {e}")
+                continue
+            
+            # Pretty print if needed
+            if print_contents:
+                print(json.dumps(data, indent=4))
+
+    return data
+
+def read_fluorescence(photometry_data_path):
+    try:
+        Fluorescence = pd.read_csv(photometry_data_path/'Processed_Fluorescence.csv', skiprows=1, index_col=False)
+    except FileNotFoundError:
+        Fluorescence = pd.read_csv(photometry_data_path/'Fluorescence.csv', skiprows=1, index_col=False)
+        
+    if 'Unnamed: 5' in Fluorescence.columns: Fluorescence = Fluorescence.drop(columns='Unnamed: 5')
+    return Fluorescence
+
+def read_fluorescence_events(photometry_data_path):
+    Events = pd.read_csv(photometry_data_path/'Events.csv', skiprows=0, index_col=False)
+    return Events
+
 def load_register_paths(dataset_path):
     
     if not os.path.exists(dataset_path/'HarpDataH1') or not os.path.exists(dataset_path/'HarpDataH2'):
@@ -133,13 +327,15 @@ def load_register_paths(dataset_path):
     h2_files = [f for f in h2_files if f.split('_')[0] == 'HarpDataH2']
     h2_dict = {int(filename.split('_')[1]):h2_folder/filename for filename in h2_files}
     
-    #print(f'Dataset {dataset_path.name} contains following registers:')
-    #print(f'H1: {list(h1_dict.keys())}')
-    #print(f'H2: {list(h2_dict.keys())}')
+    print(f'Dataset {dataset_path.name} contains following registers:')
+    print(f'H1: {list(h1_dict.keys())}')
+    print(f'H2: {list(h2_dict.keys())}')
     
     return h1_dict, h2_dict
 
 def load_registers(dataset_path):
+
+    start_time = time.time()  # Use time.time() instead of time()
     
     h1_dict, h2_dict = load_register_paths(dataset_path)
     
@@ -178,135 +374,10 @@ def load_registers(dataset_path):
                 h1_data_streams[stream_name] = pd.Series(data=stream.values.squeeze(), index=stream.index)
             except:
                 print(f'ERROR: Attempted to convert the loaded register "{stream_name}" to pandas.Series common format, but failed. Likely cause is that it has more than a single column.')
+ 
+    print(f'Registers loaded in {time.time() - start_time:.2f} seconds.')
     
     return {'H1': h1_data_streams, 'H2': h2_data_streams}
-
-def read_ExperimentEvents(path):
-    filenames = os.listdir(path/'ExperimentEvents')
-    filenames = [x for x in filenames if x[:16]=='ExperimentEvents'] # filter out other (hidden) files
-    sorted_filenames = pd.to_datetime(pd.Series([x.split('_')[1].split('.')[0] for x in filenames])).sort_values()
-    read_dfs = []
-    try:
-        for row in sorted_filenames:
-            read_dfs.append(pd.read_csv(path/'ExperimentEvents'/f"ExperimentEvents_{row.strftime('%Y-%m-%dT%H-%M-%S')}.csv"))
-        return pd.concat(read_dfs).reset_index().drop(columns='index')
-    except pd.errors.ParserError as e:
-        filename = f"ExperimentEvents_{row.strftime('%Y-%m-%dT%H-%M-%S')}.csv"
-        print(f'Tokenisation failed for file "{filename}".\n')
-        print(f'Exact description of error: {e}')
-        print('Likely due to extra commas in the "Value" column of ExperimentEvents. Please manually remove and run again.')
-        return None
-    except Exception as e:
-        print('Reading failed:', e)
-        return None
-
-def read_OnixDigital(path):
-    filenames = os.listdir(path/'OnixDigital')
-    filenames = [x for x in filenames if x[:11]=='OnixDigital'] # filter out other (hidden) files
-    sorted_filenames = pd.to_datetime(pd.Series([x.split('_')[1].split('.')[0] for x in filenames])).sort_values()
-    read_dfs = []
-    for row in sorted_filenames:
-        read_dfs.append(pd.read_csv(path/'OnixDigital'/f"OnixDigital_{row.strftime('%Y-%m-%dT%H-%M-%S')}.csv"))
-    return pd.concat(read_dfs).reset_index().drop(columns='index')
-
-def read_OnixAnalogFrameCount(path):
-    filenames = os.listdir(path/'OnixAnalogFrameCount')
-    filenames = [x for x in filenames if x[:20]=='OnixAnalogFrameCount'] # filter out other (hidden) files
-    sorted_filenames = pd.to_datetime(pd.Series([x.split('_')[1].split('.')[0] for x in filenames])).sort_values()
-    read_dfs = []
-    for row in sorted_filenames:
-        read_dfs.append(pd.read_csv(path/'OnixAnalogFrameCount'/f"OnixAnalogFrameCount_{row.strftime('%Y-%m-%dT%H-%M-%S')}.csv"))
-    return pd.concat(read_dfs).reset_index().drop(columns='index')
-
-def read_OnixAnalogData(dataset_path, binarise=False):
-    # https://github.com/neurogears/vestibular-vr/blob/benchmark-analysis/Python/vestibular-vr/analysis/round_trip.py
-    # https://open-ephys.github.io/onix-docs/Software%20Guide/Bonsai.ONIX/Nodes/AnalogIODevice.html
-    start_time = time()
-    arrays_to_concatenate = []
-    files_to_read = [x for x in os.listdir(dataset_path/'OnixAnalogData')]
-    
-    def extract_number(filename):
-        return int(filename.split('_')[-1].split('.')[0])
-    
-    # Sort the files based on the extracted number
-    files_to_read.sort(key=extract_number)
-    
-    for filename in files_to_read:
-        with open(dataset_path/'OnixAnalogData'/filename, 'rb') as f:
-            photo_diode = np.fromfile(f, dtype=np.int16)
-
-            try:
-                photo_diode = np.reshape(photo_diode, (-1,12))
-            except:
-                print(f'ERROR: Cannot reshape loaded "{filename}" binary file into [-1, 12] shape. Continuing with non-reshaped data.')
-            
-            arrays_to_concatenate.append(photo_diode)
-
-    photo_diode = np.concatenate(arrays_to_concatenate)
-    
-    if binarise:
-        PHOTODIODE_THRESHOLD = 120
-        photo_diode[np.where(photo_diode <= PHOTODIODE_THRESHOLD)] = 0
-        photo_diode[np.where(photo_diode > PHOTODIODE_THRESHOLD)] = 1
-        photo_diode = photo_diode.astype(bool)
-
-    print(f'OnixAnalogData loaded in {time() - start_time:.2f} seconds.')
-
-    return photo_diode
-
-def read_OnixAnalogClock(dataset_path):
-    start_time = time()
-    arrays_to_concatenate = []
-    files_to_read = [x for x in os.listdir(dataset_path/'OnixAnalogClock')]
-    
-    def extract_number(filename):
-        return int(filename.split('_')[-1].split('.')[0])
-    
-    # Sort the files based on the extracted number
-    files_to_read.sort(key=extract_number)
-    
-    for filename in files_to_read:
-        with open(dataset_path/'OnixAnalogClock'/filename, 'rb') as f:
-            clock_data = np.fromfile(f, dtype=np.uint64)
-            arrays_to_concatenate.append(clock_data) 
-    
-    output = np.concatenate(arrays_to_concatenate)
-
-    print(f'OnixAnalogClock loaded in {time() - start_time:.2f} seconds.')
-
-    return output
-
-def read_SessionSettings(dataset_path, print_contents=False):
-    
-    # File path to the jsonl file
-    path = Path(dataset_path)
-    jsonl_file_path = dataset_path/'SessionSettings'/os.listdir(dataset_path/'SessionSettings')[0]
-
-    # Open and read the jsonl file line by line
-    with open(jsonl_file_path, 'r') as file:
-        for line in file:
-            # Parse each line as JSON
-            data = json.loads(line)
-            
-            # Pretty print the data with correct indentation
-            pretty_data = json.dumps(data, indent=4)
-
-    if print_contents: print(pretty_data)
-
-    return data
-
-# def read_fluorescence(photometry_data_path): #FIXME needed?
-#     try:
-#         Fluorescence = pd.read_csv(photometry_data_path/'Processed_Fluorescence.csv', skiprows=0, index_col=False)
-#     except FileNotFoundError:
-#         Fluorescence = pd.read_csv(photometry_data_path/'Fluorescence.csv', skiprows=1, index_col=False)
-        
-#     if 'Unnamed: 5' in Fluorescence.columns: Fluorescence = Fluorescence.drop(columns='Unnamed: 5')
-#     return Fluorescence
-
-# def read_fluorescence_events(photometry_data_path): #FIXME needed?
-#     Events = pd.read_csv(photometry_data_path/'Events.csv', skiprows=0, index_col=False)
-#     return Events
 
 def load_streams_from_h5(data_path):
     # File path to read the HDF5 file
